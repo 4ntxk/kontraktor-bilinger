815439164fe602741e8ae3bbf33db1806b3fbe07
unknown <antonstanaszek@gmail.com>
2024-03-01 14:45:26 +0100

    quickfix

--- src/components/MainUserPage.jsx
+++ src/components/MainUserPage.jsx
@@ -1,15 +1,30 @@
 import AllContractorsTable from "./ui/AllContractorsTable";
 import SingleContractorTable from "./ui/SingleContractorTable";
 import ContractorForm from "./ui/ContractorForm";
-import Test from "./ui/Test";
+import { columns } from "./ui/columns";
+import { DataTable } from "./ui/DataTable";
 
 const MainUserPage = () => {
+  function getData() {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ];
+  }
+
+  const data = getData();
   return (
     <>
       <div className="flex text-4xl justify-center text-center">
         barykada tokenowa pokonana :PPPPP
       </div>
-      <Test />
+      <DataTable columns={columns} data={data} />
       {/* <AllContractorsTable /> */}
       {/* <SingleContractorTable /> */}
       {/* <ContractorForm /> */}
